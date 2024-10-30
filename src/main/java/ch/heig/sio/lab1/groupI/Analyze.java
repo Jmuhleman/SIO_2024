803d86bdfebe174c1101d13f0a650970c3d0c598
Jmuhleman <julien.muhlemann@heig-vd.ch>
2024-10-30 12:13:36 +0100

    add analyse for stats

--- src/main/java/ch/heig/sio/lab1/groupI/Analyze.java
+++ src/main/java/ch/heig/sio/lab1/groupI/Analyze.java
@@ -2,6 +2,8 @@ package ch.heig.sio.lab1.groupI;
 
 import ch.heig.sio.lab1.display.HeuristicComboItem;
 import ch.heig.sio.lab1.groupI.heuristique.FarthestInsertion;
+import ch.heig.sio.lab1.groupI.heuristique.NearestInsertion;
+import ch.heig.sio.lab1.groupI.heuristique.RandomInsertion;
 import ch.heig.sio.lab1.tsp.TspData;
 import ch.heig.sio.lab1.tsp.TspTour;
 
@@ -25,17 +27,49 @@ public final class Analyze {
     // pcb1173   : 56892
     // nrw1379  : 56638
     // u1817 : 57201
+    String[] filePaths = {
+            "data/att532.dat",
+            "data/u574.dat",
+            "data/pcb442.dat",
+            "data/pcb1173.dat"
+    };
 
-    TspData data = TspData.fromFile("data/att532.dat");
-    var fi = new HeuristicComboItem("Farthest ins", new FarthestInsertion());
+    String[] heuristicNames = {
+            "Farthest Insertion",
+            "Nearest Insertion",
+            "Random Insertion"
+    };
+    // Load TSP datasets
+    TspData[] data = new TspData[filePaths.length];
+    for (int i = 0; i < filePaths.length; i++) {
+      data[i] = TspData.fromFile(filePaths[i]);
+    }
 
+    // Initialize heuristic algorithms
+    HeuristicComboItem[] heuristics = {
+            new HeuristicComboItem("Farthest Insertion", new FarthestInsertion()),
+            new HeuristicComboItem("Nearest Insertion", new NearestInsertion()),
+            new HeuristicComboItem("Random Insertion", new RandomInsertion())
+    };
 
-    TspTour tour = fi.computeTour(data, 0);
+    // Run statistics for each dataset
+    for (int i = 0; i < data.length; i++) {
+      TspData tspData = data[i];
+      int totalCities = tspData.getNumberOfCities();
+      System.out.printf("Dataset: %s (Total Cities: %d)%n", filePaths[i], totalCities);
 
-    System.out.println(tour.length());
+      // Perform runs for each number of cities up to the total number
+      for (int cityCount = 1; cityCount < totalCities; cityCount++) {
+        System.out.printf("  Running heuristics for %d cities:%n", cityCount);
 
-    System.out.println(tour);
+        for (int j = 0 ; j < heuristics.length ; j++) {
 
-
+          // Assuming computeTour accepts cityCount as a parameter
+          TspTour tour = heuristics[j].computeTour(tspData, cityCount);
+          System.out.printf("    Heuristic: %s, Tour Length: %d%n", heuristicNames[j], tour.length());
+        }
+        System.out.println();
+      }
+    }
   }
 }
